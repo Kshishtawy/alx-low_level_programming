3eac70c9968d492326a6133209c9121d92d31881
Kareem <kareememadelshishtawy@gmail.com>
2023-03-02 14:44:06 -0800

    auto commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
-/**
-  * print_alphabet - writes the alphabet in lowercase followed by a new line
-  *
-  */
-void print_alphabet_x10(void)
-{
-	int i, count;
 
-	for (count = 0; count < 10; count++)
+	/**
+	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+	 * followed by a new line
+	 */
+	void print_alphabet_x10(void)
 	{
-		for (i = 97; i < 123; i++)
+		char ch;
+		int i;
+
+
+		i = 0;
+
+
+		while (i < 10)
 		{
-			_putchar(i);
+			ch = 'a';
+			while (ch <= 'z')
+			{
+				_putchar(ch);
+				ch++;
+			}
+			_putchar('\n');
+			i++;
 		}
-		_putchar('\n');
 	}
-}
