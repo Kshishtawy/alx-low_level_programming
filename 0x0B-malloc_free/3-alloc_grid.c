662d7b5123cf493f6080ef372e3da81284df6aa2
Kareem <kareememadelshishtawy@gmail.com>
2023-03-14 10:21:53 -0700

    auto commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,44 +2,44 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - creates a two dimensional integer grid.
- * @width: width of the grid.
- * @height: height of the grid.
+ * alloc_grid - Creates a 2D array of integers.
  *
- * Return: If width or height is 0 or negative, or if the allocation fails - NULL.
- *         Otherwise - a pointer to the allocated grid.
+ * @width: Width of the grid.
+ * @height: Height of the grid.
+ *
+ * Return: Pointer to the 2D array, NULL on failure or invalid inputs.
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int x, y;
+        int **grid;
+        int x, y;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+        if (width <= 0 || height <= 0)
+                return (NULL);
 
-    grid = malloc(sizeof(int *) * height);
+        grid = malloc(sizeof(int *) * height);
 
-    if (grid == NULL)
-        return (NULL);
+        if (grid == NULL)
+                return (NULL);
 
-    for (x = 0; x < height; x++)
-    {
-        grid[x] = malloc(sizeof(int) * width);
-
-        if (grid[x] == NULL)
+        for (x = 0; x < height; x++)
         {
-            for (; x >= 0; x--)
-                free(grid[x]);
-            free(grid);
-            return (NULL);
+                grid[x] = malloc(sizeof(int) * width);
+
+                if (grid[x] == NULL)
+                {
+                        for (; x >= 0; x--)
+                                free(grid[x]);
+                        free(grid);
+                        return (NULL);
+                }
         }
-    }
 
-    for (x = 0; x < height; x++)
-    {
-        for (y = 0; y < width; y++)
-            grid[x][y] = 0;
-    }
+        for (x = 0; x < height; x++)
+        {
+                for (y = 0; y < width; y++)
+                        grid[x][y] = 0;
+        }
 
-    return (grid);
+        return (grid);
 }
