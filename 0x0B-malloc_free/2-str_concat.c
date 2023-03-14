bdc23151446268e9b6426344288485c069ad1cfe
Kareem <kareememadelshishtawy@gmail.com>
2023-03-14 10:07:19 -0700

    auto commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,50 +2,51 @@
 #include <stdlib.h>
 
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * str_concat - Concatenates two strings
+ *
+ * @s1: The first string to concatenate
+ * @s2: The second string to concatenate
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-    char *conct;
-    int i, ci;
+	char *conct;
+	int i = 0, ci = 0;
 
-    if (s1 == NULL)
-        s1 = "";
+	if (s1 == NULL)
+		s1 = "";
 
-    if (s2 == NULL)
-        s2 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    i = ci = 0;
+	while (s1[i] != '\0')
+		i++;
 
-    while (s1[i] != '\0')
-        i++;
+	while (s2[ci] != '\0')
+		ci++;
 
-    while (s2[ci] != '\0')
-        ci++;
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-    conct = malloc(sizeof(char) * (i + ci + 1));
+	if (conct == NULL)
+		return (NULL);
 
-    if (conct == NULL)
-        return (NULL);
+	i = ci = 0;
 
-    i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
 
-    while (s1[i] != '\0')
-    {
-        conct[i] = s1[i];
-        i++;
-    }
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+		i++, ci++;
+	}
 
-    while (s2[ci] != '\0')
-    {
-        conct[i] = s2[ci];
-        i++, ci++;
-    }
+	conct[i] = '\0';
 
-    conct[i] = '\0';
-    return (conct);
+	return (conct);
 }
