ad066a836da598f084d33a5f1d1f8a9bec13ba00
Kareem <kareememadelshishtawy@gmail.com>
2023-03-14 09:58:45 -0700

    auto commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,34 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strdup - duplicate to new memory space location
- *
- * @str: char
- *
- * Return: 0
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * Return: concat of s1 and s2
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	char *aaa;
-	int i, r = 0;
+    char *conct;
+    int i, ci;
 
-	if (str == NULL)
-		return (NULL);
+    if (s1 == NULL)
+        s1 = "";
 
-	i = 0;
-	while (str[i] != '\0')
-		i++;
+    if (s2 == NULL)
+        s2 = "";
 
-	aaa = malloc(sizeof(char) * (i + 1));
+    i = ci = 0;
 
-	if (aaa == NULL)
-		return (NULL);
+    while (s1[i] != '\0')
+        i++;
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
+    while (s2[ci] != '\0')
+        ci++;
 
-	return (aaa);
+    conct = malloc(sizeof(char) * (i + ci + 1));
+
+    if (conct == NULL)
+        return (NULL);
+
+    i = ci = 0;
+
+    while (s1[i] != '\0')
+    {
+        conct[i] = s1[i];
+        i++;
+    }
+
+    while (s2[ci] != '\0')
+    {
+        conct[i] = s2[ci];
+        i++, ci++;
+    }
+
+    conct[i] = '\0';
+    return (conct);
 }
