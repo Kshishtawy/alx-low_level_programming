eb990c2a9263c128ead10726b03d87153fd19c5b
Kareem <kareememadelshishtawy@gmail.com>
2023-03-27 16:48:21 -0700

    auto commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: The string to print.
+ */
 void puts2(char *str)
 {
     int i;
